0b40bf6189f103130607c3a5e72fd65c76078159
Chuong Ngo <cngo-github@gmail.com>
2013-09-14 20:20:05 -0400

    Added an initial implemenation of a package for communicating translate.google.com

---
+++ src/translator/server/GoogleTranslator.go
@@ -0,0 +1,28 @@
+//package GoogleTranslator
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
+
+type TranslationResult struct {
+        url, srctxt, srclang, tgttxt, tgtlang string
+}
+
+func Translate() {
+	resp, err := 
+http.Get("http://translate.google.com/translate_a/t?q=drag%20if%20you%20want%20to%20work%20on%20this%20all%20night%20ill%20drink%20some%20coffee%20its%20no%20prob&client=t&text=&sl=auto&tl=fr")
+	if err == nil {
+		contents, err := ioutil.ReadAll(resp.Body)
+
+		if err == nil {
+			fmt.Printf("Response: %s\n", contents)
+		}
+	}
+}
+
+func main() {
+	Translate()
+}
