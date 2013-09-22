3249909114847236154e2fc0774efbe8c554d88a
Chuong Ngo <cngo-github@gmail.com>
2013-09-21 23:27:37 -0400

    Added parsing and sanitizing of the return from the server.

--- src/translator/server/GoogleTranslator.go
+++ src/translator/server/GoogleTranslator.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"io/ioutil"
 	"strings"
+	"encoding/json"
 )
 
 type TranslationResult struct {
@@ -15,22 +16,70 @@ type TranslationResult struct {
 func Translate() {
 	resp, err := 
 http.Get("http://translate.google.com/translate_a/t?q=drag%20if%20you%20want%20to%20work%20on%20this%20all%20night%20ill%20drink%20some%20coffee%20its%20no%20prob&client=t&text=&sl=auto&tl=fr")
-	if err == nil {
-		contents, err := ioutil.ReadAll(resp.Body)
 
-		if err == nil {
-			fmt.Printf("Response: %s\n", contents)
-			fmt.Println(contents[0])
-//			parseTranslation(contents)
-		}
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	contents, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	parseReturn(contents)
 }
 
-func parseTranslation(result string) {
-	spl := strings.Split(result, "]")
-	fmt.Println(spl)
+func parseReturn(result []byte) {
+	result = sanitizeReturn(result, 3)
+
+	var f interface{}
+	err2 := json.Unmarshal(result, &f)
+
+                if err2 == nil {  
+                        m := f.([]interface{})
+
+                        for k, v := range m {
+                                switch vv := v.(type) {
+                                        case string:
+                                                fmt.Println(k, " is string ", vv)
+                                        case int:
+                                                fmt.Println(k, " is int ", vv)
+                                        case []interface{}:   
+                                                fmt.Println(k, " is an array ")
+                                        default: 
+                                                fmt.Println(k, " is unknown.")
+                                }
+                        }
+                        fmt.Printf("Unmarshelled: %s\n", f)
+                }
+}
+
+func sanitizeReturn(result []byte, iterations int) []byte {
+	if(iterations > 1) {
+		result = sanitizeReturn(result, iterations - 1)
+	}
+
+	str := ToGoString(result)
+	str = strings.Replace(str, ",,", ",0,", -1)
+	return []byte(str)
 }
 
 func main() {
 	Translate()
+}
+
+func ToGoString(c []byte) string {
+	n := -1
+
+	for i, b := range c {
+		if b == 0 {
+			break
+		}
+		n = i
+	}
+
+	return string(c[:n+1])
 }
