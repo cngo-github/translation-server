c5f440238b392ead065a0cee7f9f10f8ceae9abb
Chuong Ngo <cngo-github@gmail.com>
2013-09-22 20:46:11 -0400

    Started population of the TranslateResult object.

--- src/translator/server/GoogleTranslator.go
+++ src/translator/server/GoogleTranslator.go
@@ -7,17 +7,32 @@ import (
 	"io/ioutil"
 	"strings"
 	"encoding/json"
+	"time"
 )
 
-type TranslationResult struct {
-        url, srctxt, srclang, tgttxt, tgtlang string
+type TranslateError struct {
+	When time.Time
+	Message string
 }
 
-func Translate() {
-	resp, err := 
-http.Get("http://translate.google.com/translate_a/t?q=drag%20if%20you%20want%20to%20work%20on%20this%20all%20night%20ill%20drink%20some%20coffee%20its%20no%20prob&client=t&text=&sl=auto&tl=fr")
+func (e *TranslateError) Error() string {
+	return fmt.Sprintf("Error: %v, %s", e.When, e.Message)
+}
+
+type TranslateJob struct {
+        Url string
+	Srctxt string
+	Srclang string
+	Tgttxt string
+	Tgtlang string
+}
+
+func Translate(request TranslateJob) {
+	//Contact the server.
+	resp, err := http.Get(request.Url)
 
 	if err != nil {
+		//Log error
 		fmt.Println(err)
 		return
 	}
@@ -25,37 +40,88 @@ http.Get("http://translate.google.com/translate_a/t?q=drag%20if%20you%20want%20t
 	contents, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
+		//log error
 		fmt.Println(err)
 		return
 	}
 
-	parseReturn(contents)
-}
-
-func parseReturn(result []byte) {
-	result = sanitizeReturn(result, 3)
-
+	//Parse returned JSON
 	var f interface{}
-	err2 := json.Unmarshal(result, &f)
+	err = json.Unmarshal(sanitizeReturn(contents, 3), &f)
 
-                if err2 == nil {  
-                        m := f.([]interface{})
+	if err != nil {
+		//log error
+		return
+	}
 
-                        for k, v := range m {
-                                switch vv := v.(type) {
-                                        case string:
-                                                fmt.Println(k, " is string ", vv)
-                                        case int:
-                                                fmt.Println(k, " is int ", vv)
-                                        case []interface{}:   
-                                                fmt.Println(k, " is an array ")
-                                        default: 
-                                                fmt.Println(k, " is unknown.")
-                                }
-                        }
-                        fmt.Printf("Unmarshelled: %s\n", f)
-                }
+	//Extract the translated text
+	json := f.([]interface{})
+
+	arr := json
+
+	for i := 0; i < 2; i++ {
+		s, ok := arr[0].([]interface{})
+
+		if !ok {
+			//log error
+			return
+		}
+
+		arr = s
+	}
+
+	fmt.Println("Arr: ", arr[0])
+	request.Tgttxt = arr[0].(string)
+	fmt.Println("Arr: ", json[2])
+	request.Tgtlang = json[2].(string)
 }
+
+//func parseReturn(result []byte) {
+//	result = sanitizeReturn(result, 3)
+
+//	var f interface{}
+//	err := json.Unmarshal(result, &f)
+
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+
+//	m := f.([]interface{})
+
+	//Extract the translated text.
+//	arr := m
+
+//	for i := 0; i < 2; i++ {
+//		arr, ok := m[0].([]interface{})
+
+//		if !ok {
+//			fmt.Println("Not an array")
+//			return
+//		}
+//	}
+
+//	translation := Translation(0,0)
+//	fmt.Println("Arr: ", arr[0])
+
+//              if err == nil {
+  //                      m := f.([]interface{})
+
+    //                    for k, v := range m {
+      //                          switch vv := v.(type) {
+        //                                case string:
+          //                                      fmt.Println(k, " is string ", vv)
+            //                            case int:
+              //                                  fmt.Println(k, " is int ", vv)
+                //                        case []interface{}:
+                  //                              fmt.Println(k, " is an array ")
+                    //                    default:
+                      //                          fmt.Println(k, " is unknown.")
+                        //        }
+//                        }
+//                        fmt.Printf("Unmarshelled: %s\n", f)
+//                }
+//}
 
 func sanitizeReturn(result []byte, iterations int) []byte {
 	if(iterations > 1) {
@@ -68,7 +134,10 @@ func sanitizeReturn(result []byte, iterations int) []byte {
 }
 
 func main() {
-	Translate()
+	job := 
+TranslateJob{"http://translate.google.com/translate_a/t?q=drag%20if%20you%20want%20to%20work%20on%20this%20all%20night%20ill%20drink%20some%20coffee%20its%20no%20prob&client=t&text=&sl=auto&tl=fr", 
+"", "", "", ""}
+	Translate(job)
 }
 
 func ToGoString(c []byte) string {
